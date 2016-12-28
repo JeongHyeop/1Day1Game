6310d4695aa51fb94383ac4b82134f34f4d7aa4d
WorkHyeop <WorkHyeop>
2016-12-28 23:50:16 +0900

    Base 기본이고 메인의 스크립트랑 메인은 건들지 말고 만약 건들게 된다면 꼭 말해주고 건들고 앞으로 자기 씬이랑 스크립트에는 앞에 자기 이니셜 (성까지 붙여서)!! 꼭 그렇게 작업 할 것!!

---
+++ Assets/02.Scripts/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public void PJHGameButtonClick()
+    {
+        SceneManager.LoadScene("PJHInGameScene");
+    }
+    public void KJHGameButtonClick()
+    {
+        SceneManager.LoadScene("KJHInGameScene");
+    }
+    public void KJUGameButtonClick()
+    {
+        SceneManager.LoadScene("KJUInGameScene");
+    }
+    public void KSWGameButtonClick()
+    {
+        SceneManager.LoadScene("KSWInGameScene");
+    }
+    public void SDWGameButtonClick()
+    {
+        SceneManager.LoadScene("SDWInGameScene");
+    }
+}
